6465e98492f40be9876d17af85dcc7f893402a36
Alexandru Urlateanu <82050014+alexandru-urlateanu@users.noreply.github.com>
2024-03-19 03:44:18 +0200

    Update app.py

--- 2-simpleapp/app.py
+++ 2-simpleapp/app.py
@@ -1,7 +1,7 @@
 import os
 
 def main():
-    env_var = os.getenv('MY_ENV_VAR', 'User')
+    env_var = os.getenv('MY_ENV_VAR', 'Alex')
     print('Hello, ' + env_var + '!')
 
 if __name__ == "__main__":
